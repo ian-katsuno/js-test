9c881900f5f93aaeb82a4073dcdfb53e6b53427f
Ian Katsuno <ian.katsuno@gmail.com>
2018-01-19 16:43:56 -0500

    first commit of cool js project

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+// this is a single line comment
+//  use me to explain what you are thinking
+
+// console.log('something funny');
+
+/*
+var sum = 128 + 2098 + 20389;
+var average = sum / 3; 
+*/
+
+// compute the result of
+//  789 / 12345
+//  store it in a variable 
+var num = 789 / 12345;
+// console.log that variable
+console.log(num);
